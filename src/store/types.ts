5152ee6fa9900f25e83434b4a042e88ce50b90d1
kis9a <kis9ax@gmail.com>
2021-01-31 15:52:33 +0900

    feat: defin response types and WeatherReducer

---
+++ src/store/types.ts
@@ -0,0 +1,87 @@
+export const GET_WEATHER = "GET_WEATHER";
+export const SET_LOADING = "SET_LOADING";
+export const SET_ERROR = "SET_ERROR";
+export const SET_ALEART = "SET_ALEART";
+
+export interface Weather {
+  description: string;
+  icon: string;
+  id: number;
+  main: string;
+}
+
+export interface WeatherData {
+  base: string;
+  clouds: {
+    all: number;
+  };
+  cod: number;
+  coord: {
+    lon: number;
+    lat: number;
+  };
+  dt: number;
+  id: number;
+  main: {
+    feels_like: number;
+    humidity: number;
+    pressure: number;
+    temp: number;
+    temp_max: number;
+    temp_main: number;
+  };
+  name: string;
+  sys: {
+    country: string;
+    id: number;
+    sunrise: number;
+    sunset: number;
+    type: number;
+  };
+  timezon: number;
+  visibility: number;
+  weather: Weather[];
+  wind: {
+    speed: number;
+    deg: number;
+  };
+}
+
+export interface WeatherError {
+  cod: string;
+  message: string;
+}
+
+export interface WeatherState {
+  data: WeatherData | null;
+  loading: boolean;
+  error: string;
+}
+
+interface GetWeatherAction {
+  type: typeof GET_WEATHER;
+  payload: WeatherData;
+}
+
+interface SetLoadingAction {
+  type: typeof SET_LOADING;
+}
+
+interface SetErrorAcrtion {
+  type: typeof SET_ERROR;
+  payload: string;
+}
+
+export type WeatherAction =
+  | GetWeatherAction
+  | SetLoadingAction
+  | SetErrorAcrtion;
+
+export interface AlertAcriont {
+  type: typeof SET_ALEART;
+  payload: string;
+}
+
+export interface AlertState {
+  message: string;
+}
